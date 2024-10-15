c3cd74b00a00a21f27f508a1dcb7fa9a8b41342b
kisxo <udaysubba2004@gmail.com>
2024-10-15 21:42:29 +0530

    added update-location route and view

--- locationapp/views.py
+++ locationapp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
+from .models import User
+import json
 
 @login_required
 def index(request):
-    return render(request, "locationapp/base.html")
+    users = User.objects.filter(isprivate=True)
+    print(users)
+    return render(request, "locationapp/index.html")
+
+@login_required
+def updatelocation(request):
+    if request.method != "POST" or not request.POST:
+        return HttpResponseBadRequest()
+    in_updatelocation_data = json.loads(request.body.decode())
+    print(in_updatelocation_data)
+    return HttpResponseBadRequest()
