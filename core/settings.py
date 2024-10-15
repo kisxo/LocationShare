86658413853ff6ff42a481cb598a59bfbec8f0bc
kisxo <udaysubba2004@gmail.com>
2024-10-15 18:29:02 +0530

    add custom user

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'allauth.account',
 ]
 
+AUTH_USER_MODEL = 'locationapp.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- locationapp/models.py
+++ locationapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import time
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+    time = models.DateTimeField(auto_now_add = True)
+    isprivate = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.id}, {self.username}, {self.latitude}, {self.longitude}, {self.time}, {self.isprivate}'
